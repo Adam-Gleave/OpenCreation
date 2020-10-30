d76c7763141fb1adacd77c2893af0458049cadd0
Adam-Gleave <adamg108@hotmail.co.uk>
2020-10-30 23:05:09 +0000

    Implement ImGui DockSpace

--- openck/editor/src/main.rs
+++ openck/editor/src/main.rs
@@ -7,24 +7,14 @@ fn main() {
 
     system.imgui.style_mut().window_rounding = 0f32;
     system.imgui.io_mut().config_flags |= imgui::ConfigFlags::DOCKING_ENABLE;
-    system.imgui.io_mut().docking_with_shift = true;
-
-    let (x, y) = system.display.get_framebuffer_dimensions();
-    println!("{}, {}", x, y);
 
     let mut first_open = true;
     let mut about_open = true;
 
     system.main_loop(move |_, ui| {
-        Window::new(im_str!("Scene"))
-            .flags(imgui::WindowFlags::NO_BRING_TO_FRONT_ON_FOCUS)
-            .build(ui, || {});
-        Window::new(im_str!("Tree View"))
-            .flags(imgui::WindowFlags::NO_BRING_TO_FRONT_ON_FOCUS)
-            .build(ui, || {});
-        Window::new(im_str!("Scene Objects"))
-            .flags(imgui::WindowFlags::NO_BRING_TO_FRONT_ON_FOCUS)
-            .build(ui, || {});
+        DockSpace::new(im_str!("Dockspace"))
+            .flags(DockNodeFlags::NO_RESIZE)
+            .over_viewport();
 
         if let Some(menu_bar) = ui.begin_main_menu_bar() {
             if let Some(menu) = ui.begin_menu(im_str!("File"), true) {
@@ -37,29 +27,9 @@ fn main() {
             menu_bar.end(ui);
         }
 
-        if first_open {
-            imgui::Dock::new().build(|root| {
-                root.size([1280f32, 800f32]).position([0f32, 18f32]).split(
-                    imgui::Direction::Left, 
-                    0.3f32,
-                    |left| {
-                        left.dock_window(im_str!("Tree View"));
-                    }, |right| {
-                        right.split(
-                            imgui::Direction::Up, 
-                            0.7f32,
-                            |up| {
-                                up.dock_window(im_str!("Scene"));
-                            }, |down| {
-                                down.dock_window(im_str!("Scene Objects"));
-                            })
-                    })
-            });
-        }
-
         if about_open {
             Window::new(im_str!("About"))
-                .size([300f32, 100f32], Condition::Always)
+                .size([300f32, 100f32], Condition::Appearing)
                 .opened(&mut about_open)
                 .build(ui, || {
                     ui.text("OpenCK v0.0.1");
