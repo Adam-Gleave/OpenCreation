c9efb2091e12c68772ec423233cf0e865abcd609
Gleave <adam.gleave@spirent.com>
2020-10-30 16:31:35 +0000

    Get ImGUI configured

---
+++ openck/editor/src/main.rs
@@ -0,0 +1,24 @@
+mod imgui_util;
+
+use imgui::*;
+
+fn main() {
+    let system = imgui_util::init(file!());
+
+    system.main_loop(move |_, ui| {
+        Window::new(im_str!("Hello world"))
+            .collapsible(false)
+            .size([300.0, 110.0], Condition::FirstUseEver)
+            .build(ui, || {
+                ui.text(im_str!("Hello world!"));
+                ui.text(im_str!("こんにちは世界！"));
+                ui.text(im_str!("This...is...imgui-rs!"));
+                ui.separator();
+                let mouse_pos = ui.io().mouse_pos;
+                ui.text(format!(
+                    "Mouse Position: ({:.1},{:.1})",
+                    mouse_pos[0], mouse_pos[1]
+                ));
+            });
+    });
+}
